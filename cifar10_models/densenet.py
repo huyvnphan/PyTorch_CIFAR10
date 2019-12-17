f5d6b6e0773d0427291834ea27e9b68bc2826542
Mario Geiger <geiger.mario@gmail.com>
2019-12-17 11:08:28 +0100

    init densenet

--- cifar10_models/densenet.py
+++ cifar10_models/densenet.py
@@ -120,10 +120,12 @@ class DenseNet(nn.Module):
 def _densenet(arch, growth_rate, block_config, num_init_features, pretrained, progress, device, **kwargs):
     model = DenseNet(growth_rate, block_config, num_init_features, **kwargs)
     if pretrained:
-        from torch.hub import load_state_dict_from_url
         url = "https://github.com/mariogeiger/PyTorch-CIFAR10/releases/download/1.3/{}.pt".format(arch)
-        state_dict = load_state_dict_from_url(url, map_location='cpu', progress=progress)
-        model.load_state_dict(state_dict)
+    else:
+        url = "https://github.com/mariogeiger/PyTorch-CIFAR10/releases/download/1.3/init_{}.pt".format(arch)
+    from torch.hub import load_state_dict_from_url
+    state_dict = load_state_dict_from_url(url, map_location='cpu', progress=progress)
+    model.load_state_dict(state_dict)
     return model
 
 
